6739eaca992008c5417473b0287555c55be846ff
Roscoe Hart <roscoe.hart@me.com>
2017-10-20 17:35:04 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <div className="App-header">
+                    <h2>Welcome to React!</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
